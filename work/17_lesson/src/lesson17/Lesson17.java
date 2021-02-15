89b337ae7e7cd82e9af58991fb8bcbec875572d4
Dzmitry Rafalovich <work.d.rafalovich@gmail.com>
2021-02-15 23:47:39 +0300

    lesson 17

--- work/17_lesson/src/lesson17/Lesson17.java
+++ work/17_lesson/src/lesson17/Lesson17.java
@@ -2,13 +2,88 @@ package lesson17;
 
 import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Lesson17 {
 
 
     public static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(3);
+        set.add(3);
+        set.add(3);
+        Set<String> set2 = new HashSet<>();
+        set2.add("1");
+        set2.add("1");
+        set2.add("1");
+
+        System.out.println(set2);
+
+        Set<Product> products = new HashSet<>();
+        Product product1 = new Product("Milk", 200, 0.9);
+        Product product2 = new Product("Meet", 500, 0.5);
+        Product product4 = new Product("Meet", 500, 0.5);
+//        Product product3 = product2;
+        products.add(product1);
+        products.add(product2);
+//        product1.setName("abc");
+        Product product3 = product1;
+        products.add(product4);
+        products.add(product3);
+        System.out.println(products);
+        System.out.println("Product 2 equals product 4 = "
+                + product2.equals(product4));
+
+
+        Set<Product> set1 = new HashSet<>(products);
+        Set<Product> set3 = new HashSet<Product>(20, (float) 0.5);
+
+//        Product product7 = null;
+//        Product product8 = null;
+//        product7.equals(product8);
+
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(3);
+        treeSet.add(17);
+        treeSet.add(5);
+        treeSet.add(2);
+        for (Integer i : treeSet) {
+            System.out.println(i);
+        }
+        System.out.println(treeSet);
+
+        Set<String> treeSet2 = new TreeSet<>();
+        treeSet2.add("aa");
+        treeSet2.add("Aa");
+        treeSet2.add("e");
+        treeSet2.add("aaa");
+        treeSet2.add("C");
+        treeSet2.add("b");
+        System.out.println(treeSet2);
+
+
+        Product product11 = new Product("Milk", 200, 0.9);
+        Product product12 = new Product("Meet", 500, 0.5);
+        Product product13 = new Product("Bred", 100, 0.65);
+        Product product14 = new Product("Meet", 500, 0.4);
+        Product product15 = new Product("Oil", 800, 1.0);
+        Set<Product> productSet = new TreeSet<>();
+        productSet.add(product11);
+        productSet.add(product12);
+        productSet.add(product13);
+        productSet.add(product14);
+        productSet.add(product15);
+        System.out.println(productSet);
+
+
         LocalDate date = LocalDate.now();
         LocalDate secondDate = LocalDate.of(2020, 12, 3);
         date.isAfter(secondDate);
--- work/17_lesson/src/lesson17/Product.java
+++ work/17_lesson/src/lesson17/Product.java
@@ -1,10 +1,109 @@
 package lesson17;
 
-public class Product {
+import java.util.Objects;
+
+public class Product implements Comparable<Product> {
 
     private String name;
 
     private int priceInCents;
 
     private double weight;
+
+    private String blabla;
+
+    public Product(String name, int priceInCents, double weight) {
+        this.name = name;
+        this.priceInCents = priceInCents;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPriceInCents() {
+        return priceInCents;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPriceInCents(int priceInCents) {
+        this.priceInCents = priceInCents;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getBlabla() {
+        return blabla;
+    }
+
+    public void setBlabla(String blabla) {
+        this.blabla = blabla;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (! (o instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) o;
+        if (this == o) {
+            return true;
+        }
+        boolean equals = false;
+        equals = this.priceInCents == product.priceInCents
+                && this.weight == product.weight
+                && (this.name == null ?
+                product.name == null :
+                this.name.equals(product.name));
+        return equals;
+    }
+
+    @Override
+    public int hashCode() {
+        int hashCode = 17;
+        hashCode = 31 * hashCode + priceInCents;
+        hashCode = 31 * hashCode + (int) weight;
+        hashCode = 31 * hashCode + (name == null ? 0 : name.hashCode());
+        return hashCode;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", priceInCents=" + priceInCents +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Product product) {
+        if(name == null) {
+            return 0;
+        }
+        int value = name.compareTo(product.name); //1
+        if (value == 0) { // 2
+            value = priceInCents - product.priceInCents;
+        }
+        if (value == 0) {
+            //0.5 0.37  1.99 1.0
+            value = (int) (weight * 1000 - product.weight * 1000);
+        }
+        return value;
+    }
 }
